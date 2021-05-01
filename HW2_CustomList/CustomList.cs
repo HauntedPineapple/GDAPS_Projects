08722e7edb77f904b59e4fe332de85fc81091683
Audrey Main <meilong.main@gmail.com>
2021-05-01 12:02:29 -0400

    Started implementation of CustomList class

--- HW2_CustomList/CustomList.cs
+++ HW2_CustomList/CustomList.cs
@@ -8,5 +8,62 @@ namespace HW2_CustomList
 {
     class CustomList <T>
     {
+        private T[] array;
+        private int count;
+        public int Count { get { return count; } }
+        public int Capacity { get { return array.Length; } }
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= count)
+                {
+                    return default(T);
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= count)
+                {
+                    return;
+                }
+                array[index] = value;
+            }
+        }
+
+        public CustomList()
+        {
+
+        }
+
+        public void Add(T item)
+        {
+
+        }
+        public int IndexOf(T item)
+        {
+            return -1;
+        }
+        public bool Contains(T item)
+        {
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+
+        }
+        public bool Remove(T item)
+        {
+            return false;
+        }
+        public void Insert(int index,T item)
+        {
+
+        }
+        public void Clear()
+        {
+
+        }
     }
 }
