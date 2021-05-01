bc794066102662657c6cd133e95fa938e0f9f6fa
Audrey Main <meilong.main@gmail.com>
2021-04-30 20:54:28 -0400

    Finished part 1

--- HW2_CustomList/Program.cs
+++ HW2_CustomList/Program.cs
@@ -10,6 +10,231 @@ namespace HW2_CustomList
     {
         static void Main(string[] args)
         {
+            bool isUsing = true;
+            bool validInput = false;
+            int index = 0;
+            string userInput = "";
+
+            while (!validInput)
+            {
+                Console.Write("Please enter the desired size of your list: ");
+                userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out int size))
+                {
+                    validInput = true;
+                    break;
+                }
+                Console.WriteLine("ERROR: Invalid input! Please try again!");
+            }
+
+            List<string> words = new List<string>(int.Parse(userInput));
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Commands: ");
+            Console.WriteLine("- done   - print");
+            Console.WriteLine("- indexOf   - contains");
+            Console.WriteLine("- removeAt   - remove");
+            Console.WriteLine("- insert   - clear");
+            Console.WriteLine("- get   - set");
+            Console.WriteLine("- help");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            while (isUsing)
+            {
+                validInput = false;
+                Console.Write("Enter a word or command: ");
+                userInput = Console.ReadLine();
+
+                switch (userInput.ToLower())
+                {
+                    case "done":
+                        Console.WriteLine("Goodbye!");
+                        isUsing = false;
+                        break;
+
+                    case "print":
+                        Console.WriteLine("The list contains: ");
+                        for (int i = 0; i < words.Count; i++)
+                        {
+                            Console.WriteLine("   " + words[i]);
+                        }
+                        break;
+
+                    case "indexOf":
+                        Console.Write("Please enter the word you wish to find the index of: ");
+                        userInput = Console.ReadLine();
+                        if (words.Contains(userInput))
+                        {
+                            Console.WriteLine("The index of " + userInput + " is at " + words.IndexOf(userInput));
+                        }
+                        else
+                        {
+                            Console.WriteLine("That word is not in this list");
+                        }
+                        break;
+
+                    case "contains":
+                        Console.Write("Please enter the word to search for: ");
+                        userInput = Console.ReadLine();
+                        if (words.Contains(userInput))
+                        {
+                            Console.WriteLine("That word IS in this list");
+                        }
+                        else
+                        {
+                            Console.WriteLine("That word is NOT in this list");
+                        }
+                        break;
+
+                    case "removeAt":
+                        while (!validInput)
+                        {
+                            Console.Write("Please enter the index you wish to remove: ");
+                            userInput = Console.ReadLine();
+                            if (int.TryParse(userInput, out index))
+                            {
+                                validInput = true;
+                                break;
+                            }
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Invalid input! Please try again!");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                        index = int.Parse(userInput);
+
+                        if (words.Count > index || index < 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.WriteLine("ERROR: Invalid index");
+                            break;
+                        }
+
+                        words.RemoveAt(index);
+                        Console.WriteLine("Removed " + userInput + " from the list.");
+                        break;
+
+                    case "remove":
+                        Console.Write("Please enter the word you wish to remove: ");
+                        userInput = Console.ReadLine();
+                        if (words.Contains(userInput))
+                        {
+                            words.Remove(userInput);
+                            Console.WriteLine("Removed " + userInput + " from the list.");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: The list does not contain the word " + userInput);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                        break;
+
+                    case "insert":
+                        Console.Write("Please enter the word you wish to add: ");
+                        userInput = Console.ReadLine();
+
+                        string number = "";
+                        while (!validInput)
+                        {
+                            Console.Write("Please enter the index you wish to add your word at: ");
+                            number = Console.ReadLine();
+                            if (int.TryParse(number, out index))
+                            {
+                                validInput = true;
+                                break;
+                            }
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Invalid input! Please try again!");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                        index = int.Parse(number);
+
+                        Console.WriteLine("Added \"" + userInput + "\" to the list at index " + index + ".");
+                        words.Insert(index, userInput);
+                        break;
+
+                    case "clear":
+                        Console.WriteLine("Cleared list");
+                        words.Clear();
+                        break;
+
+                    case "get":
+                        while (!validInput)
+                        {
+                            Console.Write("Please enter the index you wish to retrieve: ");
+                            userInput = Console.ReadLine();
+                            if (int.TryParse(userInput, out index))
+                            {
+                                validInput = true;
+                                break;
+                            }
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Invalid input! Please try again!");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                        index = int.Parse(userInput);
+
+                        if (words.Count > index || index < 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Invalid index");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            break;
+                        }
+
+                        Console.WriteLine("The word at index "+index+"is "+words[index]);
+                        break;
+
+                    case "set":
+                        while (!validInput)
+                        {
+                            Console.Write("Please enter the index you wish to set: ");
+                            userInput = Console.ReadLine();
+                            if (int.TryParse(userInput, out index))
+                            {
+                                validInput = true;
+                                break;
+                            }
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Invalid input! Please try again!");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                        index = int.Parse(userInput);
+
+                        if (words.Count > index || index < 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Invalid index"); 
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            break;
+                        }
+
+                        Console.Write("Please enter the word you wish to add: ");
+                        userInput = Console.ReadLine();
+
+                        Console.WriteLine("Set the word at index "+index+" to "+userInput);
+                        break;
+
+                    case "help":
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Commands: ");
+                        Console.WriteLine("- done   - print");
+                        Console.WriteLine("- indexOf   - contains");
+                        Console.WriteLine("- removeAt   - remove");
+                        Console.WriteLine("- insert   - clear");
+                        Console.WriteLine("- get   - set");
+                        Console.WriteLine("- help");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    default:
+                        words.Add(userInput);
+                        Console.WriteLine("Added " + userInput + " to the list.");
+                        break;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
