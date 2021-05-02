46908455f1e2ca47a5ddaaa68101a39d008ce9e8
Audrey Main <meilong.main@gmail.com>
2021-05-01 21:23:10 -0400

    Finished Dungeon class Finished creation of the dungeon in main

--- HW1_Dungeon/Dungeon.cs
+++ HW1_Dungeon/Dungeon.cs
@@ -8,23 +8,107 @@ namespace HW1_Dungeon
 {
     class Dungeon
     {
+        private int width, height;
 
-        public Dungeon()
+        public Dungeon(string name, int _width = 50, int _height = 30)
         {
+            Console.Title = name;
 
+            if (_width > 100)
+            {
+                _width = 100;
+            }
+            if (_width < 25)
+            {
+                _width = 25;
+            }
+
+            if (_height > 50)
+            {
+                _height = 50;
+            }
+            if (_height < 15)
+            {
+                _height = 15;
+            }
+
+            width = _width;
+            Console.WindowWidth = _width;
+            height = _height;
+            Console.WindowHeight = _height;
         }
 
-        public void DrawWallVertical(int x, int y, int length)
+        public void DrawWallVertical(int x, int y, int length, ConsoleColor color = ConsoleColor.White, char symbol = '║')
         {
+            if (x < 0 || y < 0 || length == 0
+                || x > width || y > height)
+            {
+                return;
+            }
 
+            for (int i = 0; i < Math.Abs(length); i++)
+            {
+                if (y > height || y < 0)
+                {
+                    return;
+                }
+                Console.CursorTop = y;
+                Console.CursorLeft = x;
+                Console.ForegroundColor = color;
+                Console.Write(symbol);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                if (length > 0)
+                {
+                    y++;
+                }
+                else
+                {
+                    y--;
+                }
+            }
         }
-        public void DrawWallHorizontal(int x, int y, int length)
-        {
 
+        public void DrawWallHorizontal(int x, int y, int length, ConsoleColor color = ConsoleColor.White, char symbol = '=')
+        {
+            if (x < 0 || y < 0 || length == 0
+                || x > width || y > height)
+            {
+                return;
+            }
+
+            for (int i = 0; i < Math.Abs(length); i++)
+            {
+                if (x > width || x < 0)
+                {
+                    return;
+                }
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.ForegroundColor = color;
+                Console.Write(symbol);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                if (length > 0)
+                {
+                    x++;
+                }
+                else
+                {
+                    x--;
+                }
+            }
         }
-        public void DrawObject(int x, int y, char symbol)
+        public void DrawObject(int x, int y, char symbol, ConsoleColor color = ConsoleColor.White)
         {
+            if (x < 0 || y < 0 || x > width || y > height)
+            {
+                return;
+            }
 
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+            Console.ForegroundColor = color;
+            Console.Write(symbol);
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
--- HW1_Dungeon/Program.cs
+++ HW1_Dungeon/Program.cs
@@ -22,6 +22,47 @@ namespace HW1_Dungeon
 
         static void Main(string[] args)
         {
+            Dungeon dungeon = new Dungeon("Level 1", 50, 30);
+            #region Walls
+            dungeon.DrawObject(4, 4, '╔');
+            dungeon.DrawWallVertical(4, 5, 17);
+            dungeon.DrawWallHorizontal(5, 4, 16);
+            dungeon.DrawObject(20, 4, '╗');
+
+            dungeon.DrawObject(20, 12, '╚');
+            dungeon.DrawWallVertical(20, 5, 7);
+            dungeon.DrawWallHorizontal(21, 12, 8);
+            dungeon.DrawObject(28, 12, '╝');
+
+            dungeon.DrawObject(28, 4, '╔');
+            dungeon.DrawWallVertical(28, 5, 7);
+            dungeon.DrawWallHorizontal(29, 4, 18);
+            dungeon.DrawObject(46, 4, '╗');
+
+            dungeon.DrawObject(46, 26, '╝');
+            dungeon.DrawWallVertical(20, 19, 3);
+            dungeon.DrawWallHorizontal(45, 26, -19);
+            dungeon.DrawObject(26, 26, '╚');
+
+            dungeon.DrawObject(26, 18, '╗');
+            dungeon.DrawWallVertical(26, 19, 7);
+            dungeon.DrawWallHorizontal(25, 18, -6);
+            dungeon.DrawObject(20, 18, '╔');
+
+            dungeon.DrawObject(20, 22, '╝');
+            dungeon.DrawWallVertical(46, 5, 21);
+            dungeon.DrawWallHorizontal(19, 22, -16);
+            dungeon.DrawObject(4, 22, '╚');
+            #endregion
+
+            dungeon.DrawObject(8, 12, '@', ConsoleColor.Green);
+            dungeon.DrawObject(34, 8, 'W', ConsoleColor.Cyan);
+            dungeon.DrawObject(32, 16, 'M', ConsoleColor.Red);
+            dungeon.DrawObject(38, 20, 'P', ConsoleColor.Magenta);
+            dungeon.DrawObject(40, 12, '*', ConsoleColor.Yellow);
+            dungeon.DrawObject(41, 11, '*', ConsoleColor.Yellow);
+            dungeon.DrawObject(42, 12, '*', ConsoleColor.Yellow);
+
             Console.ReadLine();
         }
     }
